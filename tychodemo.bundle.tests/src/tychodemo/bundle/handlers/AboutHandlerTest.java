c5ce793f76778684cb73c5dde8d747fcaef1da84
sudr <to_sud@yahoo.com>
2012-09-25 22:51:37 -0500

    making test pass

--- tychodemo.bundle.tests/src/tychodemo/bundle/handlers/AboutHandlerTest.java
+++ tychodemo.bundle.tests/src/tychodemo/bundle/handlers/AboutHandlerTest.java
@@ -3,12 +3,13 @@ package tychodemo.bundle.handlers;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
 
 public class AboutHandlerTest {
 
 	@Test
 	public void testGetGreeting() {
-		fail("Not yet implemented");
+		assertThat(new AboutHandler().getGreeting(), JUnitMatchers.containsString("Tycho Demo"));
 	}
 
 }
