d427d6358ec5c273541b53d6d5693ecba3ccf4dc
sudr <to_sud@yahoo.com>
2012-09-25 22:25:22 -0500

    adding test

---
+++ tychodemo.bundle.tests/src/tychodemo/bundle/handlers/AboutHandlerTest.java
@@ -0,0 +1,14 @@
+package tychodemo.bundle.handlers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AboutHandlerTest {
+
+	@Test
+	public void testGetGreeting() {
+		fail("Not yet implemented");
+	}
+
+}
